bfc347207472ce7057e856ea574f3bf0d0e45280
Parkreiner <throwawayclover@gmail.com>
2023-02-16 02:01:08 -0500

    feat: LocalStorage done?

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,11 +1,29 @@
-import { GiftIdea } from "../sharedTypesAndConstants";
+import { GiftIdea, isGiftIdea } from "../sharedTypesAndConstants";
+
+const giftKey = "gift-scouter-giftIdeas";
 
 export function writeGiftsToLocalStorage(gifts: readonly GiftIdea[]): void {
   const serialized = JSON.stringify(gifts);
-  window.localStorage.setItem("gift", serialized);
+  window.localStorage.setItem(giftKey, serialized);
 }
 
-// Not implmeneted yet
 export function getGiftsFromLocalStorage(): readonly GiftIdea[] {
-  return [];
+  const retrieved = window.localStorage.getItem(giftKey);
+
+  try {
+    if (!retrieved) throw new Error("No valid value retrieved");
+    const parsed: unknown = JSON.parse(retrieved);
+
+    if (!Array.isArray(parsed) || !parsed.every(isGiftIdea)) {
+      throw new Error("Parsed value is not a valid array");
+    }
+
+    return parsed;
+  } catch (err) {
+    console.error(err);
+
+    const fallbackArray: readonly GiftIdea[] = [];
+    writeGiftsToLocalStorage(fallbackArray);
+    return fallbackArray;
+  }
 }
--- src/sharedTypesAndConstants.ts
+++ src/sharedTypesAndConstants.ts
@@ -5,3 +5,31 @@ export type GiftIdea = Readonly<{
   price: number;
   tags: readonly string[];
 }>;
+
+export function isGiftIdea(value: unknown): value is GiftIdea {
+  if (!value || typeof value !== "object") {
+    return false;
+  }
+
+  for (const key in value) {
+    if (typeof key === "symbol") {
+      return false;
+    }
+  }
+
+  const recast = value as Record<string, unknown>;
+
+  return (
+    "description" in recast &&
+    typeof recast.description === "string" &&
+    "for" in recast &&
+    typeof recast.for === "string" &&
+    "link" in recast &&
+    typeof recast.link === "string" &&
+    "price" in recast &&
+    typeof recast.price === "number" &&
+    "tags" in recast &&
+    Array.isArray(recast.tags) &&
+    recast.tags.every((v) => typeof v === "string")
+  );
+}
